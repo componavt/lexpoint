f027c7cd7576bd45f1312e165a458e76358a1e9b
julia <julia>
2015-10-23 23:25:16 +0300

    Safe config files

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'lexpoint'),
-            'username'  => env('DB_USERNAME', 'lexpoint'),
-            'password'  => env('DB_PASSWORD', 'lexpointwlog'),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
